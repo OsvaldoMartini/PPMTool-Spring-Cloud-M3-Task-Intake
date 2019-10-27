5591b1db927ae662934c2167cd174730861d82fb
OsvaldoMartini <osvaldo.martini@gmail.com>
2019-10-27 20:59:34 +0100

    PPMTool Intake

---
+++ src/main/java/com/shifthunter/ppmtool/PpmToolSpringCloudM3TaskIntakeApplication.java
@@ -0,0 +1,13 @@
+package com.shifthunter.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmToolSpringCloudM3TaskIntakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmToolSpringCloudM3TaskIntakeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/shifthunter/ppmtool/TaskProcessor.java
@@ -0,0 +1,42 @@
+package com.shifthunter.ppmtool;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.cloud.task.launcher.TaskLaunchRequest;
+import org.springframework.messaging.support.GenericMessage;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableBinding(Source.class)  //Bridging between here and the 'BUS' RabbitMQ source class for the stream
+public class TaskProcessor {
+
+	@Autowired
+	private Source source;
+
+	// Publish the Message to the Queue
+	public void publishRequest(String payload) {
+
+		//maven://[groupid]:[artifactid]:jar:[version]
+		//String url = "maven://pluralsight.demo:pluralsight-springcloud-m3-task:jar:0.0.1-SNAPSHOT";
+		String url = "maven://com.shifthunter:ppmtool-task:jar:0.0.1-SNAPSHOT";
+
+		List<String> input = new ArrayList<String>(Arrays.asList(payload.split(",")));
+
+		TaskLaunchRequest request = new TaskLaunchRequest(url, input, null, null);
+
+		System.out.println("created task launch request ...");
+
+		// I am sending this message to the Source -> (RabbitMQ Exchange)
+		GenericMessage<TaskLaunchRequest> message = new GenericMessage<TaskLaunchRequest>(request);
+		
+		
+		
+		this.source.output().send(message);
+	}
+
+}
