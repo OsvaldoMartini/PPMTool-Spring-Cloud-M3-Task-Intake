3156610c3fb3f5f4f60b85abbe1ef8829d615ec0
Osvaldo Martini <maos@corner.ch>
2019-11-07 11:36:04 +0100

    Renaming class

--- src/main/java/com/shifthunter/ppmtool/PPMToolTask_Intake.java
+++ src/main/java/com/shifthunter/ppmtool/PPMToolTask_Intake.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PPMToolTaskIntake {
+public class PPMToolTask_Intake {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PPMToolTaskIntake.class, args);
+		SpringApplication.run(PPMToolTask_Intake.class, args);
 	}
 
 }
